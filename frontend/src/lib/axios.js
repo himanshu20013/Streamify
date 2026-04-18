8d2560d2d97be0f81e0b461d33af6a63e367d9e9
himanshu20013 <hg9490724@gmail.com>
2026-04-18 15:13:09 +0530

    frontend changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.BACKEND_URL}/api` : "/api"
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
